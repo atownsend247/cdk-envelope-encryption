12fc2bc27969cbb3a76073b781034aced2194475
Darkninja462 <Andrew@ExclusiveWebSystems.com>
2021-02-09 20:46:44 +0000

    Initial commit creating the keys

--- bin/cdk-envelope-encryption.ts
+++ bin/cdk-envelope-encryption.ts
@@ -3,4 +3,34 @@ import * as cdk from '@aws-cdk/core';
 import { CdkEnvelopeEncryptionStack } from '../lib/cdk-envelope-encryption-stack';
 
 const app = new cdk.App();
-new CdkEnvelopeEncryptionStack(app, 'CdkEnvelopeEncryptionStack');
+new CdkEnvelopeEncryptionStack(
+    app, 
+    'CdkEnvelopeEncryptionStackPrimary',
+    {
+        env: {
+            region: 'eu-west-1'
+        },
+        description: 'Envryption Stack in Primary Region'
+    }
+);
+new CdkEnvelopeEncryptionStack(
+    app, 
+    'CdkEnvelopeEncryptionStackSecondary',
+    {
+        env: {
+            region: 'eu-west-2'
+        },
+        description: 'Encryption Stack in Secondary Region'
+    }
+);
+
+new CdkEnvelopeEncryptionStack(
+    app, 
+    'CdkEnvelopeEncryptionStackTertiary',
+    {
+        env: {
+            region: 'us-east-1'
+        },
+        description: 'Encryption Stack in Tertiary Region'
+    }
+);
--- lib/cdk-envelope-encryption-stack.ts
+++ lib/cdk-envelope-encryption-stack.ts
@@ -1,18 +1,19 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as kms from '@aws-cdk/aws-kms';
 
 export class CdkEnvelopeEncryptionStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkEnvelopeEncryptionQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const key = new kms.Key(
+      this, 
+      'MyKey',
+      {
+        enableKeyRotation: true
+      }
+    );
+    key.addAlias('alias/envelope');
 
-    const topic = new sns.Topic(this, 'CdkEnvelopeEncryptionTopic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
